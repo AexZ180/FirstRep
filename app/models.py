c146810413e9524bd8c86053b8e4e85f55f9c8db
AexZ180 <alexandrozuniga92@gmail.com>
2026-03-28 12:37:18 -0500

    Add CI pipeline with Github Actions

--- app/models.py
+++ app/models.py
@@ -1,5 +1,15 @@
 from .extensions import db
 
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    onboardings = db.relationship("Onboarding", backref="user", lazy=True)
+
+
 class Onboarding(db.Model):
     __tablename__ = "onboarding"
 
@@ -8,6 +18,8 @@ class Onboarding(db.Model):
     weight = db.Column(db.Integer, nullable=False)
     days_per_week = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 
 class WorkoutPlan(db.Model):
